5c369858ca3d5e9ab6e0096a28624ff05b225970
kkranzfelder <kkranzfelder@gmail.com>
2022-04-22 18:33:48 -0400

    Figured out data formatting and began trying to strategize for messages.

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -5,9 +5,42 @@
 var Messages = {
 
   // TODO: Define how you want to store your messages.
-  _data: null,
+  _data: {}, // obj?
+  // push
+  // push: function () {
+  // Parse.readAll(successCB, errorCB);
+  // Store result in _data
+  // }
+  // pop
+  // delete
+
+
 
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
 
 };
+
+Parse.readAll(function (returnedData) {
+  console.log(JSON.stringify(returnedData));
+});
+/* [
+{"message_id":59689,
+"roomname":"the USA",
+"text":"test",
+"username":"sweet!",git
+"github_handle":"NaNdyKing",
+"campus":"rfe",
+"created_at":"2022-04-22T21:29:01.571Z",
+"updated_at":"2022-04-22T21:29:01.571Z"},
+{
+  "message_id":59688,
+  "roomname":"test",
+  "text":"Our test message",
+  "username":"Dude41a239",
+  "github_handle":"JakeReid53",
+  "campus":"rfe",
+  "created_at":"2022-04-22T21:28:08.938Z",
+  "updated_at":"2022-04-22T21:28:08.938Z"
+}
+] */
